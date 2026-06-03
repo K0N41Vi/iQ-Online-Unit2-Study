666a61de990c5ab6d490779d6cfd755be81dfdf8
K0N41Vi <jakarateship@gmail.com>
2026-06-03 13:36:49 +0900

    Create data.py

---
+++ data.py
@@ -0,0 +1,217 @@
+VOCAB_DATA = [
+    {
+        "word": "automate",
+        "meaning": "自動化する",
+        "synonyms": [
+            "mechanize",
+            "computerize"
+        ]
+    },
+
+    {
+        "word": "benefits",
+        "meaning": "利益・恩恵",
+        "synonyms": [
+            "advantages"
+        ]
+    },
+
+    {
+        "word": "discrimination",
+        "meaning": "差別",
+        "synonyms": [
+            "prejudice",
+            "bias"
+        ]
+    },
+
+    {
+        "word": "expertise",
+        "meaning": "専門知識",
+        "synonyms": [
+            "knowledge",
+            "skill",
+            "experience"
+        ]
+    },
+
+    {
+        "word": "gig",
+        "meaning": "仕事",
+        "synonyms": [
+            "job",
+            "task",
+            "assignment"
+        ]
+    },
+
+    {
+        "word": "job security",
+        "meaning": "雇用の安定",
+        "synonyms": [
+            "stable employment",
+            "secure job"
+        ]
+    },
+
+    {
+        "word": "labor",
+        "meaning": "労働",
+        "synonyms": [
+            "work",
+            "workforce",
+            "employment"
+        ]
+    },
+
+    {
+        "word": "on the margins",
+        "meaning": "社会から疎外された",
+        "synonyms": [
+            "excluded",
+            "marginalized"
+        ]
+    },
+
+    {
+        "word": "substitute",
+        "meaning": "置き換える",
+        "synonyms": [
+            "replace",
+            "exchange"
+        ]
+    },
+
+    {
+        "word": "take advantage of",
+        "meaning": "利用する",
+        "synonyms": [
+            "use",
+            "utilize"
+        ]
+    },
+
+    {
+        "word": "the other end of the spectrum",
+        "meaning": "反対側",
+        "synonyms": [
+            "opposite side"
+        ]
+    },
+
+    {
+        "word": "wage",
+        "meaning": "賃金",
+        "synonyms": [
+            "pay",
+            "salary",
+            "income"
+        ]
+    },
+
+    {
+        "word": "accumulate",
+        "meaning": "蓄積する",
+        "synonyms": [
+            "collect",
+            "gather"
+        ]
+    },
+
+    {
+        "word": "algorithm",
+        "meaning": "アルゴリズム",
+        "synonyms": [
+            "method",
+            "procedure"
+        ]
+    },
+
+    {
+        "word": "at stake",
+        "meaning": "危険にさらされて",
+        "synonyms": [
+            "at risk",
+            "in danger"
+        ]
+    },
+
+    {
+        "word": "degree of",
+        "meaning": "程度",
+        "synonyms": [
+            "level of"
+        ]
+    },
+
+    {
+        "word": "demand",
+        "meaning": "需要",
+        "synonyms": [
+            "need",
+            "requirement",
+            "desire"
+        ]
+    },
+
+    {
+        "word": "entry-level",
+        "meaning": "初心者向け",
+        "synonyms": [
+            "beginner"
+        ]
+    },
+
+    {
+        "word": "freelancer",
+        "meaning": "フリーランス",
+        "synonyms": [
+            "self-employed worker"
+        ]
+    },
+
+    {
+        "word": "hypothetical",
+        "meaning": "仮説上の",
+        "synonyms": [
+            "theoretical",
+            "imagined"
+        ]
+    },
+
+    {
+        "word": "scraps",
+        "meaning": "残り物",
+        "synonyms": [
+            "leftovers",
+            "remains"
+        ]
+    },
+
+    {
+        "word": "supply",
+        "meaning": "供給",
+        "synonyms": [
+            "stock",
+            "availability"
+        ]
+    },
+
+    {
+        "word": "take a cut",
+        "meaning": "取り分を得る",
+        "synonyms": [
+            "take a share"
+        ]
+    },
+
+    {
+        "word": "transaction",
+        "meaning": "取引",
+        "synonyms": [
+            "deal",
+            "exchange",
+            "trade"
+        ]
+    }
+]
